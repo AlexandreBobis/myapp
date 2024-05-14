374539435fb8f805fc43eeaea4983e57470e405e
Julien NGUYEN <nguyen.julien2003@gmail.com>
2024-05-14 11:51:58 +0200

    dlc calculé sur les cartes et couleurs selon temps restant

--- lib/presentation/main_page_screen/main_page_screen.dart
+++ lib/presentation/main_page_screen/main_page_screen.dart
@@ -10,6 +10,9 @@ import 'models/mainpage_item_model.dart';
 import 'provider/main_page_provider.dart';
 import 'widgets/mainpage_item_widget.dart';
 import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart'; // Import for date formatting
 
 class MainPageScreen extends StatefulWidget {
   const MainPageScreen({Key? key}) : super(key: key);
@@ -26,21 +29,55 @@ class MainPageScreen extends StatefulWidget {
 }
 
 class MainPageScreenState extends State<MainPageScreen> {
+  List _products = [];
+
   @override
   void initState() {
     super.initState();
     _readJson();
   }
 
-  List _products = []; // Initialisez _items avec une liste vide
-
   Future<void> _readJson() async {
     final String response = await rootBundle.loadString('assets/mock/products.json');
     final data = await json.decode(response);
-      setState(() {
+    setState(() {
       _products = data["products"];
-      });
+    });
+  }
+
+  // Function to calculate the difference in days between two dates
+  int _calculateDaysDifference(String dlc) {
+    final dateFormat = DateFormat('dd/MM/yy');
+    final dlcDate = dateFormat.parse(dlc); // Parse DLC string to DateTime
+    final today = DateTime.now();
+
+    // Remove time components from dates for accurate comparison
+    final normalizedDlcDate = DateTime(dlcDate.year, dlcDate.month, dlcDate.day);
+    final normalizedToday = DateTime(today.year, today.month, today.day);
+
+    final difference = normalizedDlcDate.difference(normalizedToday).inDays;
+
+    if (normalizedDlcDate.isAtSameMomentAs(normalizedToday)) {
+      return 0; // Dates are the same, return 0 days difference
+    } else {
+      return difference.abs(); // Return absolute difference in days
     }
+  }
+
+  // Function to determine color based on days difference
+  Color _getColorByDaysDifference(int daysDifference) {
+    if (daysDifference <= 2) {
+      return Colors.red;
+    } else if (daysDifference <= 4) {
+      return Colors.orange;
+    } else if (daysDifference <= 7) {
+      return Colors.yellow;
+    } else if (daysDifference <= 14) {
+      return Colors.green;
+    } else {
+      return Colors.blue;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,87 +92,80 @@ class MainPageScreenState extends State<MainPageScreen> {
               child: const Text('Load Data'),
               onPressed: _readJson,
             ),
-
-            // Display the data loaded from products.json
             _products.isNotEmpty
                 ? Expanded(
               child: ListView.builder(
                 itemCount: _products.length,
                 itemBuilder: (context, index) {
-                  return InkWell( // Wrap the Card with InkWell
+                  final dlc = _products[index]["dlc"] as String;
+                  final daysDifference = _calculateDaysDifference(dlc);
+                  final color = _getColorByDaysDifference(daysDifference);
+                  final textColor = color.computeLuminance() < 0.5?Colors.white:Colors.black;
+
+                  return InkWell(
                     onTap: () {
-                      // Navigate to detail_page_screen
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => DetailsPageScreen(
-                              //product: _products[index]
-                          ),
+                          builder: (context) => DetailsPageScreen(),
                         ),
                       );
                     },
                     child: Card(
                       margin: const EdgeInsets.all(8),
                       shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                          color: Colors.blue,
-                          width: 2, //<-- SEE HERE
-                        ),
+                        side: BorderSide(color: color, width: 2),
                         borderRadius: BorderRadius.circular(12.0),
                       ),
                       child: Stack(
                         children: [
-                          // Contenu principal de la Card
                           ListTile(
                             leading: SizedBox(
                               width: 70.0,
                               height: 70.0,
-                              child: Image.network(
-                                _products[index]["image"],
-                              ),
+                              child: Image.network(_products[index]["image"]),
                             ),
                             title: Text(_products[index]["name"]),
                             subtitle: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                Text(
-                                  "Quantité : ${_products[index]["quantity"].toString()}",
-                                ),
-                                Text("DLC : ${_products[index]["dlc"]}"),
+                                Text("Quantité : ${_products[index]["quantity"].toString()}"),
+                                Text("DLC : $dlc"),
                               ],
                             ),
                           ),
-                          // Positionnement de MyArc à l'extrémité du bord droit
-                            Positioned(
-                              right: 0, // Ajustez selon vos besoins
-                              top: 0, // Ajustez selon vos besoins
-                              bottom: 0, // Ajustez selon vos besoins
-
-                              child: ClipPath(
-                                child: Container(
-                                  padding: EdgeInsets.all(16),
-                                  decoration: BoxDecoration(
-                                    border: Border(
-                                      right: BorderSide(color: Colors.blue, width: 65),
-                                    ),
+                          Positioned(
+                            right: 0,
+                            top: 0,
+                            bottom: 0,
+                            child: ClipPath(
+                              child: Container(
+                                padding: EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    right: BorderSide(color: color, width: 65),
                                   ),
-
                                 ),
-                                clipper: ShapeBorderClipper(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(12))),
+                              ),
+                              clipper: ShapeBorderClipper(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
                               ),
                             ),
+                          ),
                           Positioned(
                             right: 10,
-                            top: 22, // Ajustez selon vos besoins
-                            child: Text("+99 j",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 22,
-                            )
-                          ))
+                            top: 22,
+                            child: Text(
+                              "${daysDifference} j",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: textColor,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -150,9 +180,6 @@ class MainPageScreenState extends State<MainPageScreen> {
     );
   }
 }
-
-
-
 
 
   /// Section Widget
