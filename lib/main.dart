3f0fabdda582f703dd9dffa97c681d6365d95bb0
AlexandreBobis <alexandrebobis91@gmail.com>
2024-05-21 10:10:08 +0200

    Refacto

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,44 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'core/app_export.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Supabase.initialize(
+      url: "https://dkfcfptrfrwksqbyqqlo.supabase.co",
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrZmNmcHRyZnJ3a3NxYnlxcWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3MDAzNzAsImV4cCI6MjAzMTI3NjM3MH0.swXCEi4lZ_1Iws8V802o3-cH-QHXnH59CR8UkKtNa08"
+  );
   Future.wait([
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
     PrefUtils().init()
   ]).then((value) {
     runApp(MyApp());
   });
+
+  /* WidgetsFlutterBinding.ensureInitialized();
+  try {
+    print('Current working directory: ${Directory.current.path}');
+    await dotenv.load(fileName: ".env");
+    if (dotenv.env['DATABASE_URL']!= null && dotenv.env['API_KEY']!= null) {
+      String url = dotenv.env['DATABASE_URL']!;
+      String anonKey = dotenv.env['ANON_KEY']!;
+      await Supabase.initialize(url: url, anonKey: anonKey);
+    } else {
+      print("Missing required environment variables.");
+    }
+    Future.wait([
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
+      PrefUtils().init()
+    ]).then((value) {
+      runApp(MyApp());
+    });
+  } catch (e) {
+    print("error loading environment variables: $e");
+  } */
 }
 
 class MyApp extends StatelessWidget {
